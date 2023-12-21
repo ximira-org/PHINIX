ab923595e2ccb3da1789b1cdb82a31e1b0546fc1
jaggik <jagadishkmahendran@gmail.com>
2023-12-20 21:52:57 -0500

    simplified launch file with new custom ROS wrapper for OAK camera

--- src/launch/phinix_launch/launch/phinix.launch.py
+++ src/launch/phinix_launch/launch/phinix.launch.py
@@ -6,75 +6,10 @@ from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, Opaq
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 from launch.conditions import IfCondition
-from launch_ros.actions import ComposableNodeContainer, Node
-from launch_ros.descriptions import ComposableNode
-
-
-def launch_setup(context, *args, **kwargs):
-    log_level = 'info'
-    if(context.environment.get('DEPTHAI_DEBUG')=='1'):
-        log_level='debug'
-
-    urdf_launch_dir = os.path.join(get_package_share_directory('depthai_descriptions'), 'launch')
-    
-    params_file = LaunchConfiguration("params_file")
-    camera_model = LaunchConfiguration('camera_model',  default = 'OAK-D')
-
-    name = LaunchConfiguration('name').perform(context)
-
-    parent_frame = LaunchConfiguration('parent_frame',  default = 'oak-d-base-frame')
-    cam_pos_x    = LaunchConfiguration('cam_pos_x',     default = '0.0')
-    cam_pos_y    = LaunchConfiguration('cam_pos_y',     default = '0.0')
-    cam_pos_z    = LaunchConfiguration('cam_pos_z',     default = '0.0')
-    cam_roll     = LaunchConfiguration('cam_roll',      default = '0.0')
-    cam_pitch    = LaunchConfiguration('cam_pitch',     default = '0.0')
-    cam_yaw      = LaunchConfiguration('cam_yaw',       default = '0.0')
-    
-    return [
-            Node(
-                condition=IfCondition(LaunchConfiguration("use_rviz").perform(context)),
-                package="rviz2",
-                executable="rviz2",
-                name="rviz2",
-                output="log",
-                arguments=["-d", LaunchConfiguration("rviz_config")],
-            ),
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(
-                os.path.join(urdf_launch_dir, 'urdf_launch.py')),
-            launch_arguments={'tf_prefix': name,
-                              'camera_model': camera_model,
-                              'base_frame': name,
-                              'parent_frame': parent_frame,
-                              'cam_pos_x': cam_pos_x,
-                              'cam_pos_y': cam_pos_y,
-                              'cam_pos_z': cam_pos_z,
-                              'cam_roll': cam_roll,
-                              'cam_pitch': cam_pitch,
-                              'cam_yaw': cam_yaw}.items()),
-
-        ComposableNodeContainer(
-            name=name+"_container",
-            namespace="",
-            package="rclcpp_components",
-            executable="component_container",
-            composable_node_descriptions=[
-                    ComposableNode(
-                        package="depthai_ros_driver",
-                        plugin="depthai_ros_driver::Camera",
-                        name=name,
-                        parameters=[params_file],
-                    )
-            ],
-            arguments=['--ros-args', '--log-level', log_level],
-            output="both",
-        ),
-
-    ]
+from launch_ros.actions import  Node
 
 
 def generate_launch_description():
-    depthai_prefix = get_package_share_directory("depthai_ros_driver")
 
     tts_balacoon_pkg_prefix = get_package_share_directory('phinix_tts_balacoon')
     tts_balacoon_node_param_file = os.path.join(
@@ -85,25 +20,9 @@ def generate_launch_description():
         description='tts settings'
     )
 
-    declared_arguments = [
-        DeclareLaunchArgument("name", default_value="oak"),
-        DeclareLaunchArgument("parent_frame", default_value="oak-d-base-frame"),
-        DeclareLaunchArgument("cam_pos_x", default_value="0.0"),
-        DeclareLaunchArgument("cam_pos_y", default_value="0.0"),
-        DeclareLaunchArgument("cam_pos_z", default_value="0.0"),
-        DeclareLaunchArgument("cam_roll", default_value="0.0"),
-        DeclareLaunchArgument("cam_pitch", default_value="0.0"),
-        DeclareLaunchArgument("cam_yaw", default_value="0.0"),
-        DeclareLaunchArgument("params_file", default_value=os.path.join(depthai_prefix, 'config', 'camera.yaml')),
-        DeclareLaunchArgument("use_rviz", default_value='false'),
-        DeclareLaunchArgument("rviz_config", default_value=os.path.join(depthai_prefix, "config", "rviz", "rgbd.rviz"))
-    ]
-
-    ld = LaunchDescription(declared_arguments + [OpaqueFunction(function=launch_setup)])
-
-    phinix_obstacle_det_node = Node(
-        package="depthmap_gpu",
-        executable="depthmap"
+    phinix_obstacle_detector_node = Node(
+        package="phinix_obstacle_detector",
+        executable="phinix_obstacle_detector_py_exe"
     )
 
     phinix_sensor_abstractor_node = Node(
@@ -127,13 +46,15 @@ def generate_launch_description():
         parameters=[LaunchConfiguration('tts_balacoon_node_param_file')],
     )
 
-    ld.add_action(phinix_obstacle_det_node)
-    ld.add_action(tts_balacoon_param)
-    ld.add_action(phinix_sensor_abstractor_node)
-    ld.add_action(phinix_text_detector_node)
-    ld.add_action(phinix_tts_simulator_node)
-    ld.add_action(phinix_tts_balacoon_node)
+    ld = [
+        phinix_obstacle_detector_node,
+        tts_balacoon_param,
+        phinix_text_detector_node,
+        phinix_tts_simulator_node,
+        phinix_tts_balacoon_node,
+        phinix_sensor_abstractor_node
+    ]
 
-    return ld
+    return LaunchDescription(ld)
 
 
